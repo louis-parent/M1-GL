b8f3ca0067f2087c3ff4fc3d950491fc76aaf90e
Louis Parent <contact@louis-parent.fr>
2022-03-24 11:14:37 +0100

    Fin requête recherche recette Symfony HAI806I

--- Semestre2/HAI806I-ArchitecturesAvanceesWeb/Symfony/api/src/Controller/RecipeController.php
+++ Semestre2/HAI806I-ArchitecturesAvanceesWeb/Symfony/api/src/Controller/RecipeController.php
@@ -8,15 +8,69 @@
 
 	
 	class RecipeController extends AbstractController{
-		public function search(String $name, String $author, int $difficulty, String $ingredient, float $price) : Array{
+		public function search(String $name, String $author, String $difficulty, String $ingredient, String $price) : Array{
             $em = $this->getDoctrine()->getManager();
-            $query = $em->createQuery(
-            'SELECT r
-                FROM App\Entity\Recipe r
-                WHERE r.name like :name AND r.difficulty <= :difficulty
-                ORDER BY r.name'
-            )->setParameter("name", "%" . $name . "%")
-            ->setParameter("difficulty", $difficulty);
+            
+            $dql = "SELECT r
+		            FROM App\Entity\Recipe r
+					JOIN App\Entity\User u
+					WHERE 1 = 1";
+					
+			if($name != "undefined")
+			{
+				$dql .= "AND LOWER(r.name) LIKE LOWER(:name)";
+			}
+			
+			if($difficulty != "undefined")
+			{
+				$dql .= "AND r.difficulty <= :difficulty";
+			}
+			
+			if($author != "undefined")
+			{
+				$dql .= "AND LOWER(u.nickname) LIKE LOWER(:user)";
+			}
+			
+			if($price != "undefined")
+			{
+				$dql .= "AND :price <= (SELECT SUM(i1.price) FROM App\Entity\Ingredient i1 WHERE i1.recipe = r.id)";
+			}
+			
+			if($ingredient != "undefined")
+			{
+				$dql .= "AND EXISTS (SELECT i2.id FROM App\Entity\Ingredient i2 WHERE i2.recipe = r.id AND LOWER(i2.name) LIKE LOWER(:ingredient))";
+			}
+					
+			$dql .= "ORDER BY r.name";
+            
+            
+            
+            $query = $em->createQuery($dql);
+            
+            if($name != "undefined")
+			{
+           		$query->setParameter("name", "%" . $name . "%");
+			}
+			
+			if($difficulty != "undefined")
+			{
+            	$query->setParameter("difficulty", $difficulty);
+			}
+			
+			if($author != "undefined")
+			{
+            	$query->setParameter("user", "%" . $author . "%");
+			}
+			
+			if($price != "undefined")
+			{
+            	$query->setParameter("price", $price);
+			}
+			
+			if($ingredient != "undefined")
+			{
+            	$query->setParameter("ingredient", "%" . $ingredient . "%");
+			}
 
 			return $query->getResult();
 		}
